cdb54bb4652689be6f616c55cd300d3badc2d78f
Juan F <92638685+einenjfr@users.noreply.github.com>
2022-01-06 13:30:11 +0100

    Create bubble.c

---
+++ Order/C/numbers/bubble.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#lnum := vector &&&& size := size of lnum
+void bubble (int* lnum, int size) {
+	int aux;
+	
+	for (int i = 0; i < size; i++)
+		for (int j = i+1; j < size; j++)
+			if (lnum[i] > lnum[j]) {
+				aux = lnum[j];
+				lnum[j] = lnum[i];
+				lnum[i] = aux;
+			}
+}
+
+void bubbleFloat (float* lnm, float size) {
+	float aux;
+	
+	for (int = 0; i < size; i++)
+		for (int j = i+1; j < size; j++)
+			if (lnum[i] > lnum[j]) {
+				aux = lnum[j];
+				lnum[j] = lnum[i];
+				lnum[i] = aux;
+			}
+}
